8661458085a6954d7267277af2623c6452ca3d4e
Jonas Bondesson <jonasbondesson@yahoo.com>
2024-01-05 15:30:18 +0100

    fixed accountfragment

--- app/src/main/java/com/example/myfavouriteplaces/currentUser.kt
+++ app/src/main/java/com/example/myfavouriteplaces/currentUser.kt
@@ -1,11 +1,14 @@
 package com.example.myfavouriteplaces
 
+import com.google.android.gms.maps.model.LatLng
+
 object currentUser {
     var documentId : String? = null
     var userID: String? = null
     var name: String? = null
     var location: String? = null
     var userImage: String? = null
+    var latLng: LatLng? = null
 
     var favouritesList = mutableListOf<Place>()
 
